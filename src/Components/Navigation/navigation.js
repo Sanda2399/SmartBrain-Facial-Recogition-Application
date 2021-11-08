ab5fd28b14833457ab10298ac316b3fa3ae4aaa5
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-11-08 11:13:28 -0500

    Implemented Sign Out Placeholder

---
+++ src/Components/Navigation/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navigation.css'
+
+const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <span>Sign Out</span>
+        </nav>
+    )
+}
+
+export default Navigation;
