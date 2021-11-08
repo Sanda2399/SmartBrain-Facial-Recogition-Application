32d2ae768cf2b76b488f087deb781f72dd3c97f5
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-11-08 15:29:16 -0500

    Pre-Api Implementation
    - Search Bar and Button both have placeholder functionality to make sure they are working correctly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+// import Clarifai from 'clarifai';
 import ImageLinkForm from './Components/ImageLinkForm/imagelinkform';
 import Navigation from './Components/Navigation/navigation';
 import Ranking from './Components/Ranking/ranking';
 import React from 'react';
 
+// const app = new Clarifai.App(
+//   {
+//     apiKey: '412dc614d4fc442585f824ce6c17cbd9'
+//   }
+// );
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = (event) => {
+    console.log("click");
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Ranking />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
       </div>
     );
   }
--- src/Components/ImageLinkForm/imagelinkform.js
+++ src/Components/ImageLinkForm/imagelinkform.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './imagelinkform.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
     return (
         <div className="ILF_container">
             <span className="ILF_main-text">This Magic Brain will detect faces in your pictures. Give it a try!</span>
             <div className="ILF_submission-area">
-                <input type="text" placeholder="Place Image Link here..." className="ILF_input-box"/>
-                <button className="ILF_detect-button"><span>Detect</span></button>
+                <input type="text" placeholder="Place Image Link here..." onChange={props.onInputChange} className="ILF_input-box"/>
+                <button onClick={props.onButtonSubmit} className="ILF_detect-button"><span>Detect</span></button>
             </div>
         </div>
     )
